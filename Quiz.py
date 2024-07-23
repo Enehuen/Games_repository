bed1bdc99fc5ce9043909cbf823c558aaff7dc3b
Enehuen <animatorezequiel@gmail.com>
2024-07-23 15:43:15 -0300

    Ya casi terminado el proyecto agregue un sistema de puntos y un apartado de jugador, para que de esta forma puedan llegar a haber mas de 1, corrregido tambien los bucles y pulidos los textos procedentes de cada if.

--- Quiz.py
+++ Quiz.py
@@ -1,45 +1,55 @@
 
 
-print("Bienvenido, Empezaremos el Quiz con preguntas de cultura general.")
+name= input("Bienvenido/a, Empezaremos el Quiz con preguntas de cultura general.\n¿Podrias decirme tu nombre? ")
 
-conf = input("Si desea comenzar ingrese 'Si', en caso contrario ingrese 'No' ")
+conf = input(f"Muy bien {name}. Cada respuesta correcta suma 2 puntos y cada incorrecta resta 1 punto \nSi desea comenzar ingrese 'Si', en caso contrario ingrese 'No': ")
 def resp_correcta(respu,respc):
     if respu.lower() == respc:
         return True
     else:
         return False
 
+puntaje=0
 
-
-while conf == "Si":
-    print("Pregunta 1: ¿Cuál es el país más grande del mundo por superficie?")
-    resp1=input("Tu respuesta es: ")
+while conf.lower() == "si":
+    resp1=input("Pregunta 1: ¿Cuál es el país más grande del mundo por superficie? \nTu respuesta es: ")
     if  resp_correcta(resp1,"rusia") != False:
-        print("Respuesta 1 correcta. \nPasemos a la siguiente pregunta.")
-        resp2 = input("¿En qué año llegó el hombre a la luna?: ")
+        print(f"Respuesta correcta, {name} suma 2 puntos. \nPasemos a la siguiente pregunta.")
+        puntaje += 2
+        resp2 = input("¿En qué año llegó el hombre a la luna? \nTu respuesta es: ")
         while resp1 != True:
             if resp_correcta(resp2,"1969") != False:
-                print("Respuesta 2 correcta. \nPasemos a la siguiente pregunta.")
-                while resp2 != True:
-                    resp3 = input("¿Cuál es la capital de Australia?")
-                    if resp_correcta(resp3,"Canberra") != False:
-                        print("Respuesta 3 correcta. \nPasemos a la siguiente pregunta.")
+                print(f"Respuesta correcta, {name} suma 2 puntos. \nPasemos a la siguiente pregunta.")
+                puntaje += 2
+                resp3 = input("¿Cuál es la capital de Australia? \nTu respuesta es: ")
+                while resp2 != False:
+                    if resp_correcta(resp3,"canberra") != False:
+                        print(f"Respuesta correcta, {name} suma 2 puntos. \nPasemos a la siguiente pregunta.")
+                        puntaje += 2
+                        resp4 = input("¿Qué vitamina se obtiene principalmente a través de la exposición al sol? \nTu respuesta es: ")
                         while resp3 != True:
-                            resp4 = input("¿Qué vitamina se obtiene principalmente a través de la exposición al sol?")
-                            if resp_correcta(resp4,"Vitamina D") != False:
-                                print("Respuesta 4 correcta. \nPasemos a la siguiente pregunta.")
+                            if resp_correcta(resp4,"vitamina d") != False:
+                                print(f"Respuesta correcta, {name} suma 2 puntos. \nPasemos a la siguiente pregunta.")
+                                puntaje += 2
+                                resp5 = input("¿Quién escribió 'Don Quijote de la Mancha'? \nTu respuesta es: ")
                                 while resp4 != True:
-                                    resp5 = input("¿Quién escribió 'Don Quijote de la Mancha'?")
-                                    if resp_correcta(resp5,"Miguel de Cervantes") != False:
-                                        print("Respuesta 5 correcta, Felicidades haz completado el Quiz")
+                                    if resp_correcta(resp5,"miguel de cervantes") != False:
+                                        puntaje += 2
+                                        print(f"Respuesta correcta, {name} suma 2 puntos. \nFelicidades haz completado el Quiz con una puntuación de {puntaje} puntos.")
+                                        quit()
                                     else:
-                                        resp5 = input("Respuesta incorrecta, Vuelva a intentarlo. \n¿Quién escribió 'Don Quijote de la Mancha'?")
+                                        puntaje -= 1
+                                        resp5 = input(f"Respuesta incorrecta, {name} pierde 1 punto,. Vuelva a intentarlo. \n¿Quién escribió 'Don Quijote de la Mancha'? \nTu respuesta es: ")
                             else: 
-                                resp4 = input("Respuesta incorrecta, Vuelva a intentarlo. \n¿Qué vitamina se obtiene principalmente a través de la exposición al sol?")
+                                puntaje -= 1
+                                resp4 = input(f"Respuesta incorrecta, {name} pierde 1 punto,. Vuelva a intentarlo. \n¿Qué vitamina se obtiene principalmente a través de la exposición al sol? \nTu respuesta es: ")
                     else:
-                        resp3 = input("Respuesta incorrecta, Vuelva a intentarlo. \n¿Cuál es la capital de Australia?")
+                        puntaje -= 1
+                        resp3 = input(f"Respuesta incorrecta, {name} pierde 1 punto,. Vuelva a intentarlo. \n¿Cuál es la capital de Australia? \nTu respuesta es: ")
             else:
-                resp2 =input("Respuesta incorrecta, Vuelva a intentarlo. \n¿En qué año llegó el hombre a la luna?: ")
+                puntaje -= 1
+                resp2 =input(f"Respuesta incorrecta, {name} pierde 1 punto,. Vuelva a intentarlo. \n¿En qué año llegó el hombre a la luna? \nTu respuesta es: ")
     else:
-        resp1 = input("Respuesta incorrecta, Vuelva a intentarlo. \nPregunta 1: ¿Cuál es el país más grande del mundo por superficie?: ")
+        puntaje -= 1
+        resp1 = input(f"Respuesta incorrecta, {name} pierde 1 punto,. Vuelva a intentarlo. \nPregunta 1: ¿Cuál es el país más grande del mundo por superficie? \nTu respuesta es:  ")
     
--- readme.txt
+++ readme.txt
@@ -3,4 +3,7 @@
 En este pequeño projecto voy a realizar un Quiz con preguntas de cultura general.
 En un principio mi idea es utilizar bucles para cada una de las 10 preguntas que integran el Quiz.
 Ya aplicadas las funciones para dejar de lado las lineas de codigo repetitivas, luego la exportare a un .py distinto para dejar solo la estructura del codigo.
+Este Quiz suma puntos conforme se responde correctamente y resta por cada respuesta incorrecta
+Lo ultimo que le quiero agregar es un contador por participantes, haciendo que hayan varios jugadores y que se realice un conteo de cada uno. 
+
 
