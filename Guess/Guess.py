baf10fdbdc28e17759336c933596316b7b7ccc27
Enehuen <animatorezequiel@gmail.com>
2024-07-29 21:24:16 -0300

    Agrego una funcion para is.digit, luego de hacer el add me di cuenta que no me toma los apartados dentro del while asique terminaria tirando error si llegan a ingresar un STR, pero lo soluciono facil en otro add

--- Guess/Guess.py
+++ Guess/Guess.py
@@ -1,13 +1,25 @@
 import random
-Num_Usuario = int(input("Bienvenido/a, Este juego es sencillo, vamos a generar un numero aleatorio entre 0 y 100. \nUsted va a intentar adivinarlo, si no lo adivina le vamos a tirar pistas para llegar a este numero. \nIngrese el numero que crea usted: "))
+def v_numero(x):
+    if x.isdigit():
+        return 1
+    else:
+        return 0
+Num_Usuario = input("Bienvenido/a, Este juego es sencillo, vamos a generar un numero aleatorio entre 0 y 100. \nUsted va a intentar adivinarlo, si no lo adivina le vamos a tirar pistas para llegar a este numero. \nIngrese el numero que crea usted: ")
 
-Num_Random = random.randrange(101)
+Num_Random = int(random.randrange(101))
 intentos = 0
-while Num_Random != Num_Usuario:
-    if Num_Usuario < Num_Random:
-        intentos+=1
-        Num_Usuario = int(input("Tu numero es más chico que el numero que intenta adivinar, intentelo nuevamente: "))
-    elif Num_Random < Num_Usuario:
-        intentos+=1
-        Num_Usuario = int(input("Tu numero es más grande que el numero que intenta adivinar, intentelo nuevamente: "))
+
+while Num_Usuario != Num_Random:
+    if v_numero(Num_Usuario) is 1:
+        Num_Usuario = int(Num_Usuario)
+        while Num_Usuario != Num_Random:
+            Num_Usuario = int(Num_Usuario)
+            if Num_Usuario < Num_Random:
+                intentos+=1
+                Num_Usuario = input("Tu numero es más chico que el numero que intenta adivinar, intentelo nuevamente: ")
+            elif Num_Random < Num_Usuario:
+                intentos+=1
+                Num_Usuario = input("Tu numero es más grande que el numero que intenta adivinar, intentelo nuevamente: ")
+    else:
+        Num_Usuario = input("Error al ingresar su numero. \ningrese un caracter numerico para empezar el juego: ")
 print(f"Felicidades el numero es: {Num_Random}, solo te tomo {intentos} intentos.")
